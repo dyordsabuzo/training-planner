880ab2e24990e47c8dccad1e803c658cd517f632
Dyords Abuzo <dyordsabuzo@gmail.com>
2024-05-30 11:01:41 +1000

    Fix noreferrer on link click

--- src/pages/ExercisePage.tsx
+++ src/pages/ExercisePage.tsx
@@ -209,6 +209,7 @@ const ExercisePage = () => {
             <a
               className="no-underline text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               target="_blank"
+              rel="noreferrer"
               href={exerciseData.videoLink}
             >
               Watch video
